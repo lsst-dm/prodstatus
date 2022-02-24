c2ebeef531494e3ad3429632fc6baf6c6f835e11
Eric Neilsen <neilsen@fnal.gov>
2022-02-24 13:16:03 -0600

    Start of Step class

---
+++ python/lsst/prodstatus/Step.py
@@ -0,0 +1,221 @@
+#
+# This file is part of prodstatus package.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Interface for managing and reporting on data processing campaigns."""
+
+# imports
+import dataclasses
+from typing import Mapping, List, Optional
+from pathlib import Path
+import yaml
+
+from lsst.prodstatus.Workflow import Workflow
+
+STEP_SPEC_FNAME = 'step.yaml'
+
+@dataclasses.dataclass
+class Step:
+    """API for managing and reporting on data processing campaigns.
+
+    Parameters
+    ----------
+    name : `str`
+        An identifier for the step.
+    split_bands : `bool`
+        Workflows in the step are split by band?
+    exposure_groups : `dict`
+        Parameterns for splitting workflows in the step by
+        exposure number.
+        (Keyword arguments to Workflow.split_by_exposure.)
+    workflows : `list` ['lsst.prodstatus.Workflow']
+        A list of workflows in the step.
+    issue_name : `str`
+        The name of the jira issue that tracks this step.
+
+    """
+
+    name: str
+    split_bands: bool = False
+    exposure_groups: Optional[Mapping[str, int]] = None
+    workflows: List[Workflow] = dataclasses.field(default_factory=list)
+    issue_name: Optional[str] = None
+
+    @classmethod
+    def generate_new(
+        cls,
+        name,
+        base_bps_config,
+        split_bands,
+        exposure_groups,
+        exposures,
+        workflow_base_name="",
+    ):
+        """Generate a new step, constructing its workflows.
+
+        Parameters
+        ----------
+        name : `str`
+            The name of the step.
+        base_bps_config : `lsst.control.bps.BpsConfig`
+            BPS configuration for the workflow.
+        split_bands : `bool`
+            Split workflows by band.
+        exposure_groups : `dict`
+            Keyword arguments to Workflow.split_by_exposure.
+        exposures : `pandas.DataFrame`
+            A DataFrame with the following columns:
+            ``"band"``
+                The filter for the exposure.
+            ``"exp_id"``
+                The exposures id
+        base_name : `str`
+            The base for the name of the workflows.
+
+        Returns
+        -------
+        step : `Step`
+            A new step with workflow instances.
+        """
+        step = cls(name, split_bands, exposure_groups)
+        step._generate_workflows(base_bps_config, exposures, workflow_base_name)
+        return step
+
+    def _generate_workflows(self, base_bps_config, exposures, base_name):
+        """Generate the workflows for this step.
+
+        Parameters
+        ----------
+        base_bps_config : `lsst.control.bps.BpsConfig`
+            BPS configuration for the workflow.
+        exposures : `pandas.DataFrame`
+            A DataFrame with the following columns:
+            ``"band"``
+                The filter for the exposure.
+            ``"exp_id"``
+                The exposures id
+        base_name : `str`
+            The base for the names of the workflows.
+
+        """
+
+        # Begin by making one workflows with does everything in this step.
+        bps_config = base_bps_config.copy()
+        bps_config["pipelineYaml"] = f"{bps_config['pipelineYaml']}#{self.name}"
+        monolithic_workflow = Workflow(
+            bps_config,
+            exposures=exposures,
+            step=self.name,
+            name=f"{base_name}_{self.name}",
+        )
+
+        # If this step is to split the workflows by band, do so
+        if self.split_bands:
+            band_workflows = monolithic_workflow.split_by_band()
+        else:
+            band_workflows = [monolithic_workflow]
+
+        # If this step is te split the workflows by exposure number, go through
+        # each workflow (already split by band if requested) and spit it further
+        # by exposure id, and add them to the instances list of workflows.
+        if self.exposure_groups is not None:
+            for workflow in band_workflows:
+                split_workflows = workflow.split_by_exposure(**self.exposure_groups)
+                self.workflows.extend(split_workflows)
+        else:
+            self.workflows = band_workflows
+
+    def to_files(self, dir):
+        """Save step data to files in a directory.
+
+
+        Parameters
+        ----------
+        dir : `pathlib.Path`
+            Directory into which to save files.
+        """
+        dir = Path(dir)
+        if self.name is not None:
+            dir = dir.joinpath(self.name)
+            dir.mkdir(exist_ok=True)
+
+        step_spec = {
+            'name': self.name,
+            'split_bands': self.split_bands,
+            'workflows': [{'name':w.name, 'issue': w.issue_name} for w in self.workflows]
+            }
+        
+        if self.exposure_groups is not None:
+            step_spec['exposure_groups'] = self.exposure_groups
+        
+        if self.issue_name is not None:
+            step_spec['issue'] = self.issue_name
+
+        step_spec_path = dir.joinpath(STEP_SPEC_FNAME)
+        with open(step_spec_path, "wt") as step_spec_io:
+            yaml.dump(step_spec, step_spec_io, indent=4)
+
+        for workflow in self.workflows:
+            workflow.to_files(dir)
+
+    @classmethod
+    def from_files(cls, dir, name=None):
+        """Load workflow data from files in a directory.
+
+        Parameters
+        ----------
+        dir : `pathlib.Path`
+            Directory into which to save files.
+        name : `str`
+            The name of the campaign (used to determine the subdirectory)
+
+        Returns
+        -------
+        campaign : `Campaign`
+            An initialized instance of a campaign.
+        """
+        dir = Path(dir)
+        if name is not None:
+            dir = dir.joinpath(name)
+
+        step_spec_path = dir.joinpath(STEP_SPEC_FNAME)
+        with open(step_spec_path, "rt") as step_spec_io:
+            step_spec = yaml.safe_load(step_spec_io)
+
+        name = name if name is not None else step_spec["name"]
+        split_bands = step_spec['split_bands']
+        
+        if 'exposure_groups' in step_spec:
+            exposure_groups = step_spec['exposure_groups']
+        else:
+            exposure_groups = {}
+        
+        if "issue_name" in step_spec:
+            issue_name = step_spec["issue_name"]
+        else:
+            issue_name = None
+
+        step = cls(name, split_bands, exposure_groups, [], issue_name)
+        for workflow_spec in step_spec['workflows']:
+            workflow = Workflow.from_files(dir, name=workflow_spec['name'])
+            step.workflows.append(workflow)
+        
+        return step
+    
---
+++ tests/test_Step.py
@@ -0,0 +1,150 @@
+# This file is part of prodstatus package.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# coding: utf-8
+"""Test Workflow."""
+
+import unittest
+from os import environ
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+import numpy as np
+import pandas as pd
+
+from lsst.ctrl.bps import BpsConfig
+from lsst.prodstatus.Step import Step
+
+BPS_CONFIG_PATH = Path(
+    environ["PRODSTATUS_DIR"], "tests", "data", "bps_config_base.yaml"
+)
+TEST_STEP_NAME = "teststep"
+TEST_WORKFLOW_BASE_NAME = "testwf"
+
+
+class TestStep(unittest.TestCase):
+    def test_generate_new(self):
+        bps_config = BpsConfig(BPS_CONFIG_PATH)
+
+        exposures = pd.DataFrame(
+            {
+                "band": ["g", "g", "r", "g", "i", "i", "r"],
+                "exp_id": [1, 3, 4, 5, 10, 11, 12],
+            }
+        )
+
+        # Test without splitting by bands or exposures
+        split_bands = False
+        exposure_groups = {}
+        step = Step.generate_new(
+            TEST_STEP_NAME,
+            bps_config,
+            split_bands,
+            exposure_groups,
+            exposures,
+            workflow_base_name=TEST_WORKFLOW_BASE_NAME,
+        )
+        self.assertEqual(len(step.workflows), 1)
+
+        # Test splitting by bands, but not exposures
+        split_bands = True
+        exposure_groups = {}
+        step = Step.generate_new(
+            TEST_STEP_NAME,
+            bps_config,
+            split_bands,
+            exposure_groups,
+            exposures,
+            workflow_base_name=TEST_WORKFLOW_BASE_NAME,
+        )
+        self.assertEqual(len(step.workflows), 6)
+
+        # Test splitting by exposures, but not bands
+        split_bands = False
+        group_size = 3
+        exposure_groups = {"group_size": group_size}
+        step = Step.generate_new(
+            TEST_STEP_NAME,
+            bps_config,
+            split_bands,
+            exposure_groups,
+            exposures,
+            workflow_base_name=TEST_WORKFLOW_BASE_NAME,
+        )
+        self.assertEqual(len(step.workflows), int(np.ceil(len(exposures) / group_size)))
+
+        # Test splitting by bands and exposures
+        split_bands = True
+        group_size = 2
+        exposure_groups = {"group_size": group_size}
+        step = Step.generate_new(
+            TEST_STEP_NAME,
+            bps_config,
+            split_bands,
+            exposure_groups,
+            exposures,
+            workflow_base_name=TEST_WORKFLOW_BASE_NAME,
+        )
+
+        # Count up how many workflows we expect in each band, and sum them
+        # to get the total workflows we expect
+        num_workflows = 0
+        for band in "ugrizy":
+            band_exposures = exposures.query(f"band == '{band}'")
+            num_workflows_in_band = int(np.ceil(len(band_exposures) / group_size))
+            if num_workflows_in_band == 0:
+                # There will always be at least one group per band?
+                num_workflows_in_band = 1
+            num_workflows += num_workflows_in_band
+        self.assertEqual(len(step.workflows), num_workflows)
+
+    def test_file_save_load(self):
+        bps_config = BpsConfig(BPS_CONFIG_PATH)
+        exposures = pd.DataFrame(
+            {
+                "band": ["g", "g", "r", "g", "i", "i", "r"],
+                "exp_id": [1, 3, 4, 5, 10, 11, 12],
+            }
+        )
+        split_bands = True
+        group_size = 2
+        exposure_groups = {"group_size": group_size}
+        step = Step.generate_new(
+            TEST_STEP_NAME,
+            bps_config,
+            split_bands,
+            exposure_groups,
+            exposures,
+            workflow_base_name=TEST_WORKFLOW_BASE_NAME,
+        )
+
+        with TemporaryDirectory() as temp_dir:
+            step_dir = Path(temp_dir)
+            step.to_files(step_dir)
+            read_step = Step.from_files(step_dir, TEST_STEP_NAME)
+
+        self.assertEqual(step.name, read_step.name)
+        self.assertEqual(step.split_bands, read_step.split_bands)
+        self.assertEqual(len(step.workflows), len(read_step.workflows))
+        for workflow, read_workflow in zip(step.workflows, read_step.workflows):
+            self.assertEqual(workflow.name, read_workflow.name)
+            self.assertEqual(workflow.step, read_workflow.step)
+            self.assertEqual(workflow.band, read_workflow.band)
+            self.assertEqual(workflow.issue_name, read_workflow.issue_name)
