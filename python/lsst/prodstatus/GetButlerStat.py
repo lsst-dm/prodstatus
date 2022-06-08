6bf5e6dae165f8fa03baa9b0ee109766a696fe7c
kuropat <kuropat@fnal.gov>
2022-06-08 15:54:38 -0500

    Switched from ButlerURI to ResourcePath

--- python/lsst/prodstatus/GetButlerStat.py
+++ python/lsst/prodstatus/GetButlerStat.py
@@ -198,7 +198,7 @@ class GetButlerStat:
                     key = sub_str.split('/')[-1]
                     "date_str = key.split('T')[0]"
                     date_stamp = datetime.datetime.strptime(key, '%Y%m%dT%H%M%SZ').tamestamp()
-                    "date_stamp = datetime.datetime.strptime(date_str, "%Y%m%d").timestamp()"
+                    """date_stamp = datetime.datetime.strptime(date_str, "%Y%m%d").timestamp()"""
                     print(f"last_stat {self.last_stat} date_stamp {date_stamp} stop_stamp {self.stop_stamp}")
                     if self.last_stat <= date_stamp <= self.stop_stamp:
                         collections.append(c)
